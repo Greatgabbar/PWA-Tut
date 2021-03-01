62ccf05df69fa747b3b3c667e7ca842bebc95f57
GreatGabbar <strivedi_be19@thapar.edu>
2021-03-01 23:55:13 +0530

    added in cache for offline mode

--- sw.js
+++ sw.js
@@ -1,4 +1,31 @@
-self.addEventListener('install',()=>{
+const staticCacheName='site-static';
+const assets=[
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  '/img/dish.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+]
+self.addEventListener('install',(e)=>{
     console.log("Service worker successfully installed!!!");
     // we can do here the caching work i.e those available for offline mode
+    e.waitUntill(
+        caches.open(staticCacheName).then(cache=>{
+            console.log('caching ...........');
+            cache.addAll(assets);
+        })
+    );
+})
+
+self.addEventListener('activate',(act)=>{
+    console.log('Service Worker successfully Activated!!!!');
+})
+
+self.addEventListener('fetch',(e)=>{
+    // console.log('fetch',e);
 })
