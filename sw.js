2ad1e6c5d3cb752ab97adbd722ee95c7ba7c59a9
GreatGabbar <strivedi_be19@thapar.edu>
2021-03-02 13:10:03 +0530

    caching done

--- sw.js
+++ sw.js
@@ -22,10 +22,15 @@ self.addEventListener('install',(e)=>{
     );
 })
 
-self.addEventListener('activate',(act)=>{
+self.addEventListener('activate',(e)=>{
     console.log('Service Worker successfully Activated!!!!');
 })
 
 self.addEventListener('fetch',(e)=>{
     // console.log('fetch',e);
+    e.respondWith(
+        caches.match(e.request).then(res=>{
+            return res || fetch(e.request);
+        })
+    )
 })
